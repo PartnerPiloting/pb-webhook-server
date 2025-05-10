8f3a10d46bfed59245bd3b48fc39aa473347044a
Guy WIlson <guyralphwilson@gmail.com>
2025-05-10 11:19:58 +1000

    Add minimal test files to diagnose require issue

--- test_queue_dispatcher.js
+++ test_queue_dispatcher.js
@@ -3,7 +3,6 @@ console.log("<<<<< TEST_QUEUE_DISPATCHER.JS - LOADED - Version D >>>>>");
 
 module.exports = function mountTestDispatcher(app) {
     console.log("<<<<< TEST_QUEUE_DISPATCHER.JS - mountTestDispatcher function CALLED >>>>>");
-    // No actual route mounting or complex logic, just a confirmation.
     if (app && typeof app.post === 'function') {
         console.log("App object was received by mountTestDispatcher.");
     } else {
