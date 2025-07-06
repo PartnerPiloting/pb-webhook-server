fd743f5d87cb56c02f0772b61c4f9a6c250aee31
Guy WIlson <guyralphwilson@gmail.com>
2025-07-06 17:58:14 +1000

    Add new Next.js project scaffold and initial component migration

---
+++ linkedin-messaging-followup-next/pages/follow-up.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+import FollowUpManager from '../components/FollowUpManager';
+
+export default function FollowUpPage() {
+  return (
+    <Layout>
+      <FollowUpManager />
+    </Layout>
+  );
+}
